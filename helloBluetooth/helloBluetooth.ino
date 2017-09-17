333ce2a0648c64fc91bc6750114385999bd01b39
Gabriel Downs <gndowns@gmail.com>
2017-09-17 19:51:34 -0400

    serial bluetooth communication! (arduino to linux)

--- helloBluetooth/helloBluetooth.ino
+++ helloBluetooth/helloBluetooth.ino
@@ -1,21 +1,22 @@
+char data;
 
-char data = 0;
 void setup() {
-  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
-
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("hello from arduino!");
 }
 
 void loop() {
-  if(Serial.available() > 0) {
+  if (Serial.available()) {
     data = Serial.read();
-    Serial.print(data);
-    Serial.print("\n");
-    if(data == '1') {
-      digitalWrite(LED_BUILTIN, HIGH);
-    }
-    else if(data == '0') {
-      digitalWrite(LED_BUILTIN, LOW);
-    }
   }
+  if (data == '1') {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else if (data == '0') {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
+  delay(100);
 }
