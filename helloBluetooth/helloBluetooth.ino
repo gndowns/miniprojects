d68b187e1256e74c7a5e69f38d9efdf988281341
Gabriel Downs <gndowns@gmail.com>
2017-09-17 23:58:36 -0400

    use different ports for bluetooth serial

--- helloBluetooth/helloBluetooth.ino
+++ helloBluetooth/helloBluetooth.ino
@@ -1,15 +1,21 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial btSerial(10, 11); //RX, TX
+
 char data;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
   while (!Serial);
-  Serial.println("hello from arduino!");
+
+  btSerial.begin(9600);
+  btSerial.println("Hello from arduino!");
 }
 
 void loop() {
-  if (Serial.available()) {
-    data = Serial.read();
+  if (btSerial.available()) {
+    data = btSerial.read();
   }
   if (data == '1') {
     digitalWrite(LED_BUILTIN, HIGH);
